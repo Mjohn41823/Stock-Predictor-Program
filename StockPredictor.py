2b948c1f51097b1a018168bd5a3c0b280543dfe6
Matt Johnson <everettmattjohnson@gmail.com>
2021-07-21 21:29:27 -0400

    Update StockPredictor.py

--- StockPredictor.py
+++ StockPredictor.py
@@ -16,3 +16,30 @@ print(plt.plot(df['Close'],label="Close Price History"))
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
+data=df.sort_index(ascending=True,axis=0)
+new_dataset=pd.DataFrame(index=range(0,len(df)))
+#Sorting date and timing
+for i in range(0,len(data)):
+    new_dataset["Date"][i]=data["Date"][i]
+    new_dataset["Close"][i]=data["Close"][i]
+#Normalize
+
+scaler=MinMaxScaler(feature_range=(0,1))
+final_dataset=new_dataset.values
+
+train_data=final_dataset[0:987,1:]
+valid_data=final_dataset[987:,:]
+
+new_dataset.index=new_dataset.Date
+new_dataset.drop("Date",axis=1,inplace=True)
+scaler=MinMaxScaler(feature_range=(0,1))
+scaled_data=scaler.fit_transform(final_dataset)
+
+x_train_data,y_train_data=[].[]
+
+for i in range(60,len(train_data)):
+    x_train_data.append(scaled_data[i-60:i,0])
+    y_train_data.append(scaled_data[i,0])
+
+x_train_data,y_train_data=np.array(x_train_data),np.array(y_train_data)
+x_train_data=np.reshape(x_train_data,(x_train_data.shape[0],x_train_data.shape[1],1))
