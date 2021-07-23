4bfbb38cdb616062f0fa77b1d95acb6f07806f12
Matt Johnson <everettmattjohnson@gmail.com>
2021-07-22 21:54:02 -0400

    Update StockPredictor.py

--- StockPredictor.py
+++ StockPredictor.py
@@ -33,6 +33,20 @@ valid_data=final_dataset[987:,:]
 new_dataset.index=new_dataset.Date
 new_dataset.drop("Date",axis=1,inplace=True)
 scaler=MinMaxScaler(feature_range=(0,1))
+
+#Build and Train the LSTM Model
+lstm_model=Sequential()
+lstm_model.add(LSTM(units=50, return_sequences=True, input_shape=
+(x_train_data.shape[1],1)))
+lstm_model.add(LSTM(units=50))
+lstm_model.add(Dense(1))
+
+inputs_data=new_dataset[len(new_dataset)-len(valid_data)-60:].values
+inputs_data=inputs_data.reshape(-1,1)
+inputs_data=scaler.transform(inputs_data)
+
+lstm_model.compile(loss='mean_squared_error',optimizer='adam')
+lstm_model.fit(x_train_data,y_train_data,epochs=1,batch_size=1,verbose=2)
 scaled_data=scaler.fit_transform(final_dataset)
 
 x_train_data,y_train_data=[].[]
